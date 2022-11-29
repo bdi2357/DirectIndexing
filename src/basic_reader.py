6d54856df659d203b0c611b88271ed2ecaf93bfa
bdi2357 <itaybendan@Itays-MBP-2.lan>
2022-11-29 20:22:51 +0200

    Improving Input reader

--- src/basic_reader.py
+++ src/basic_reader.py
@@ -35,39 +35,57 @@ def stat_cols(df):
     df["qtr_return"] = df[close_col].pct_change(Qtr_trading_days).tail()
 
 def input_reader(input_file):
+    D_input = {}
     with open(input_file,"r") as input:
         inputs = input.read().split("\n")
         inputs = [x.split(" : ") for x in inputs]
 
         inputs = {x[0]:x[1] for x in inputs}
     print(inputs, len(list(inputs.items())))
-    target_index = pd.read_csv(inputs['Target_index_returns'])
+    path_taget_index = os.path.join(*(inputs['Target_index_returns'].split("|")))
+    target_index = pd.read_csv(path_taget_index)
     stat_cols(target_index)
+    D_input["target_index"] = target_index
     print(target_index.tail())
-    print(os.listdir(inputs["Price_volume_data"])[:10])
-    print(os.listdir(inputs["Target_index_holdings"])[:10])
+    PV_data_path = os.path.join(*(inputs["Price_volume_data"].split("|")))
+    print(os.listdir(PV_data_path)[:10])
+    D_input["PV_data_path"] = PV_data_path
+    Target_index_holdings_path = os.path.join(*(inputs["Target_index_holdings"].split("|")))
+    D_input["Target_index_holdings_path"] = Target_index_holdings_path
+    print(os.listdir(Target_index_holdings_path)[:10])
     print(inputs['Universe'])
     print(inputs['Constraints'])
     print(inputs['RiskFunction'])
     print(inputs["UpdatingDates"])
-    with open(inputs["UpdatingDates"]) as dts:
+    updating_dates_path = os.path.join(*(inputs["UpdatingDates"].split("|")))
+    with open(updating_dates_path) as dts:
         dts = dts.read().split("\n")
         print([parse(x).strftime("%Y-%m-%d") for x in dts if len(x)==8])
         dts = [parse(x).strftime("%Y-%m-%d") for x in dts if len(x)==8]
         target_index = target_index.set_index("Date")
         print(target_index.loc[dts].tail())
-    constraints_files = os.listdir(inputs['Constraints'])
+    constraints_path = os.path.join(*(inputs['Constraints'].split("|")))
+    constraints = {}
+    constraints_files = os.listdir(constraints_path)
     missing = [x for x in Constraints_nms if not x in constraints_files]
     print("missing constraints are: ",missing)
     for f in constraints_files:
-        if f[-4:] == ".csv":
-            print(f,"\n",pd.read_csv(os.path.join(inputs['Constraints'],f)).head())
-        elif f[-4:] == ".txt":
-            with open(os.path.join(inputs['Constraints'],f)) as rd:
+        if f[-4:] == ".csv" and f.lower().find("sector") > -1:
+            print(f,"\n",pd.read_csv(os.path.join(constraints_path,f)).head())
+            constraints["sectors"] = pd.read_csv(os.path.join(constraints_path, f))
+            constraints["sectors"] = constraints["sectors"].set_index("Sector")["Max_Weight"].to_dict()
+
+        elif f[-4:] == ".txt" :
+            with open(os.path.join(constraints_path,f)) as rd:
                 #print(f,"\n",rd.read())
                 tickers = (rd.read().split("\n"))
                 tickers = [x for x in tickers if re.findall('[A-Z]+',x) and re.findall('[A-Z]+',x)[0] == x]
                 print(tickers)
+                constraints["forbiden_tickers"] = tickers
+    D_input["constraints"] = constraints
+    print(D_input)
+    return D_input
+
 
 
 
---
+++ src/main_test.py
@@ -0,0 +1,11 @@
+from basic_reader import input_reader
+import os,sys,re
+import numpy as np
+import pandas as pd
+from dummy_strategy import dummy_wrapper
+import time
+import glob
+from basic_stats import generate_basic_stats
+
+if __name__ == "__main__":
+    
